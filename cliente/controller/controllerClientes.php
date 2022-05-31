f19860f1a3d0f507abaf959587c7051eecef7066
VinicioJesus <viniciojesus298@gmail.com>
2022-05-31 16:57:57 -0300

    preparando controller cliente

--- cliente/controller/controllerClientes.php
+++ cliente/controller/controllerClientes.php
@@ -1,3 +1,28 @@
 <?php
 
+  function inserirCliente ($dadosCliente){
+
+    if(!empty($dadosCliente))
+    {
+
+      if(!empty($dadosCliente[0]['nome']) && !empty($dadosCliente[0]['documento']))
+      {
+        $arrayDados = array (
+          "nome"      => $dadosCliente[0]['nome'],
+          "documento"  => $dadosCliente[0]['documento']          
+      );
+
+      require_once(SRC.'model/bd/cliente.php');
+      if(insertCliente($arrayDados))
+                        return true;
+                    else
+                        return array('idErro'  => 1, 
+                                     'message' => 'Não foi possivel inserir os dados no Banco de Dados');
+      }else
+      return array('idErro'   => 2,
+                   'message'  => 'Existem campos obrigatório que não foram preenchidos.');
+    }
+
+  }
+
 ?>
---
+++ modulo/config.php
@@ -0,0 +1,37 @@
+<?php
+    /**************************************************************************************
+     * Objetivo: Arquivo responsavel pela criação de variaveis e constantes do projeto
+     * Autor: Marcel
+     * Data: 25/04/2022
+     * Versão: 1.0
+    ****************************************************************************************/
+
+    
+    
+    define('SRC', $_SERVER['DOCUMENT_ROOT'].'/estacionamento/projeto-estacionamento/');
+    
+    /***********FUNÇÕES***************/
+    //função para converter array para json 
+    function createJSON($arrayDados){
+        
+        {
+            // validação para tratar array sem dados, vazio.
+            if (!empty($arrayDados)) {
+                
+                //configura o padrão de conexão para o formato JSON
+                header('Content-Type: application/json');
+    
+                $dadosJSON = json_encode($arrayDados);
+                
+                //json_encode(); - converte o array para json
+                //json_decode(); - converte o json para array
+    
+                return $dadosJSON;
+
+            }else{
+                return false;
+            }
+        }
+        
+    }
+?>
