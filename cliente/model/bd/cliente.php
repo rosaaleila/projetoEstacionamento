e015823c7e0b227ef933077cd4cfd6b3242ca328
LEILA ROSA PEREIRA <21276364@CFP127ALUNO.LOCAL>
2022-06-01 15:45:59 -0300

    model cliente finalizada

--- cliente/model/bd/cliente.php
+++ cliente/model/bd/cliente.php
@@ -40,9 +40,134 @@
 
     }
 
-    function listarAllClientes() {
+    function listarAllClientes()
+    {
 
-        
+        // abre a conexao com o BD
+        $conexao = conexaoMysql();
+
+        // script para listar todos os dados do BD
+        $sql = "select * from tblcliente order by id desc;";
+
+        // executa o script sql no BD e guarda o retorno dos dados (se houver)
+        $result = mysqli_query($conexao, $sql);
+
+        // valida se o BD retornou registros 
+        if ($result) {
+
+            $cont = 0;
+
+            while ($rsDados = mysqli_fetch_assoc($result)) // é o mesmo que criar um cont, converter para array e guardar a qtd de itens
+            {
+
+                $arrayDados[$cont] = array(
+                    "id"        =>  $rsDados['id'],
+                    "nome"      =>  $rsDados['nome'],
+                    "documento"  =>  $rsDados['documento']
+                );
+                $cont++;
+
+            }
+
+            // solicita o fechamento da conexao com o BD
+            fecharConexaoMysql($conexao);
+            
+            if (isset($arrayDados))
+                return $arrayDados;
+            else
+                return false;
+
+        }
+
+    }
+
+    function selectByIdCliente()
+    {
+
+        // abre a conexao com o BD
+        $conexao = conexaoMysql();
+
+        // script para listar todos os dados do BD em ordem decrescente
+        $sql = "select * from tblcliente where id = " . $id;
+
+        // executa o script sql no BD e guarda o retorno dos dados (se houver)
+        $result = mysqli_query($conexao, $sql);
+
+        // valida se o BD retornou registros 
+        if ($result) {
+
+            if ($rsDados = mysqli_fetch_assoc($result)) 
+            {
+
+                $arrayDados = array(
+                    "id"        =>  $rsDados['id'],
+                    "nome"      =>  $rsDados['nome'],
+                    "documento"  =>  $rsDados['documento']
+                );
+                
+                return $arrayDados;
+            } else {
+                return false;
+            }
+
+            // solicita o fechamento da conexao com o BD
+            fecharConexaoMysql($conexao);
+
+        }
+
+    }
+
+    function updateCliente($dadosCliente)
+    {
+
+        $status = (bool) false;
+
+        //Abre a conexão com o banco de dados
+        $conexao = conexaoMysql();
+
+        //Monta o script para enviar para o BD
+        $sql = "update tblcliente set
+                        nome = '" . $dadosCliente['nome'] . "', 
+                        documento = '" . $dadosCliente['documento'] . "'
+                        where id =" . $dadosCliente['id'];
+
+        // validação para verificar se o script sql está correto
+        if (mysqli_query($conexao, $sql)) {
+            // validacao para verificar se uma linha foi acrescentada no BD
+            if (mysqli_affected_rows($conexao))
+                $status = true;
+        }
+
+        // fecha a conexao com o BD
+        fecharConexaoMysql($conexao);
+
+        return $status;
+
+    }
+
+    function deleteCliente($idCliente)
+    {
+
+        // abre a conexao com o BD
+        $conexao = conexaoMySql();
+
+        $status = (bool) false;
+
+        $sql = "delete from tblcliente where id =" . $id;
+
+        // validação para verificar se o script sql está correto para executá-lo
+        if (mysqli_query($conexao, $sql)) {
+
+            // validacao para verificar se uma linha foi acrescentada no BD
+            if (mysqli_affected_rows($conexao))
+                $status = true;
+
+        }
+
+        // fecha a conexao com o BD
+        fecharConexaoMysql($conexao);
+
+        return $status;
 
     }
 
