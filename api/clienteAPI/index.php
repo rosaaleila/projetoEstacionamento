3970f612685e6feebff92c81944798d26477c43a
VinicioJesus <viniciojesus298@gmail.com>
2022-06-01 16:39:20 -0300

    endpoint get id finalizado

--- api/clienteAPI/index.php
+++ api/clienteAPI/index.php
@@ -47,6 +47,49 @@
     }
 });
 
+  //Endpoint Requisição para listar contatos pelo id
+  $app->get('/clientes/{id}', function($request, $response, $args){
+    
+    //Recebe o id do registro que devera ser retornado pela api
+    //Esse ID está chegando pela varável criada no endpoint
+    $id = $args['id'];
+     
+     //import da controller de contatos, que fará a busca de dados
+    require_once('../modulo/config.php');
+    require_once('../cliente/controller/controllerClientes.php');
+    //solicita os dados para a controller
+    if($dados = buscarCliente($id))
+    {
+      if (!isset($dados['idErro'])) {
+        
+        //realiza a conversão do array de dados para json
+      if ($dadosJSON = createJSON($dados))
+        {   
+            // Caso exista dados a serem retornados, informamos o statusCode 200 e
+            // enviamos um JSON com todos os dados encontrados
+           return $response   ->withStatus(200)
+                              ->withHeader('Content-Type', 'application/json')
+                              ->write($dadosJSON);
+        }
+
+      }else {
+        $dadosJSON = createJSON($dados);
+
+        return $response  ->withStatus(404)
+                          ->withHeader('Content-Type', 'application/json')
+                          ->write('{"message": "Dados inválidos",
+                          "Erro": '.$dadosJSON.' }');
+                          
+      }
+    }else{
+
+      //retorna os statusCode que significa que a requisição foi aceita, porém 
+      //sem conteudo de retorno
+      return $response   ->withStatus(204);
+                  
+    }
+
+  });
 
   //Endpoint Requisição para inserir um novo cliente
   $app->post('/clientes', function($request, $response, $args){
@@ -90,70 +133,70 @@
 
   });
 
-//Endpoint Requisição para atualizar um contato, simulando o PUT
-$app->put('/clientes/{id}', function($request, $response, $args){
-    
-  //Recebe do header da requisição qual será o content type
-  $contentTypeHeader = $request->getHeaderLine('Content-Type');
-  //Cria um array ois dependendo do content-type temos mais informações separadas
-  $contentType = explode(';', $contentTypeHeader);
+  //Endpoint Requisição para atualizar um contato, simulando o PUT
+  $app->put('/clientes/{id}', function($request, $response, $args){
+      
+    //Recebe do header da requisição qual será o content type
+    $contentTypeHeader = $request->getHeaderLine('Content-Type');
+    //Cria um array ois dependendo do content-type temos mais informações separadas
+    $contentType = explode(';', $contentTypeHeader);
 
-  if(is_numeric($args['id']))
-  {
-    
-    
-    //Recebe o id enviado no Endpoint atraves da vareavel ID
-    $id = $args['id'];  
-    
-    echo($id);
-     
+    if(is_numeric($args['id']))
+    {
+      
+      
+      //Recebe o id enviado no Endpoint atraves da vareavel ID
+      $id = $args['id'];  
+      
+      echo($id);
+      
 
-    switch ($contentType[0]) {
-      case 'application/json':
-         //import da controller de contatos, que fará a busca de dados
-         require_once('../modulo/config.php');
-         require_once('../cliente/controller/controllerClientes.php');
+      switch ($contentType[0]) {
+        case 'application/json':
+          //import da controller de contatos, que fará a busca de dados
+          require_once('../modulo/config.php');
+          require_once('../cliente/controller/controllerClientes.php');
 
-        //chama a função para buscar a foto que ja está salva no banco de dados
-        
-
-        $dadosBody = $request->getParsedBody();
+          //chama a função para buscar a foto que ja está salva no banco de dados
           
-        //Cria um array com toods os dados couns e do do arquivo que será enviado para o servidor
-        $arrayDados = array( $dadosBody,                               
-                            "id" => $id                              
-        );
-       
+
+          $dadosBody = $request->getParsedBody();
+            
+          //Cria um array com toods os dados couns e do do arquivo que será enviado para o servidor
+          $arrayDados = array( $dadosBody,                               
+                              "id" => $id                              
+          );
         
-        $resposta = atualizarCliente($arrayDados);
+          
+          $resposta = atualizarCliente($arrayDados);
 
-        if (is_bool($resposta) && $resposta == true) {
+          if (is_bool($resposta) && $resposta == true) {
 
-          return $response   ->withStatus(200)
-                              ->withHeader('Content-Type', 'application/json')
-                              ->write('{"message":"Contato atualizado com sucesso"}');
+            return $response   ->withStatus(200)
+                                ->withHeader('Content-Type', 'application/json')
+                                ->write('{"message":"Contato atualizado com sucesso"}');
 
-        } elseif (is_array($resposta) && $resposta['idErro'])        
-        {
-          $dadosJSON = createJSON($resposta);
+          } elseif (is_array($resposta) && $resposta['idErro'])        
+          {
+            $dadosJSON = createJSON($resposta);
 
-          return $response   ->withStatus(400)
-                              ->withHeader('Content-Type', 'application/json')
-                              ->write('{"message":"Erro ao atualizar contato."},
-                              "Erro": '.$dadosJSON.' }');
-          break;      
-        
-        
-      } 
+            return $response   ->withStatus(400)
+                                ->withHeader('Content-Type', 'application/json')
+                                ->write('{"message":"Erro ao atualizar contato."},
+                                "Erro": '.$dadosJSON.' }');
+            break;      
+          
+          
+        } 
+      }
+    }else
+    {
+      //retorna um erro que significa que o cliente passou os dados errados
+      return  $response   ->withStatus(404)
+                          ->withHeader('Content-Type', 'application/json')
+                          ->write('{"message" : "É obrigatorio informar um ID com formato valido (número)"}');
     }
-  }else
-  {
-    //retorna um erro que significa que o cliente passou os dados errados
-    return  $response   ->withStatus(404)
-                        ->withHeader('Content-Type', 'application/json')
-                        ->write('{"message" : "É obrigatorio informar um ID com formato valido (número)"}');
-  }
-});
+  });
 
   //Executa todos os Endpoint
   $app->run();
