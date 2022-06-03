676a91910078a7cb2e5aa806ab2bbc8399e9e3f5
VinicioJesus <viniciojesus298@gmail.com>
2022-06-03 14:11:29 -0300

    controller e model vagas finalizado

---
+++ vaga/controller/controllerVagas.php
@@ -0,0 +1,164 @@
+<?php
+
+/***********************************************************************
+ * Objetivo: Arquivo responsável pela manipulação de dados de planos.
+ * 		    Obs.: Este arquivo fará a ponte entre a View e a Model.
+ * Autora: Vinicio
+ * Data: 01/06/2022
+ * Versão: 1.0
+ ***********************************************************************/
+
+// Função para receber dados da View e encaminhar para a Model (função Insert)
+function inserirVaga($dadosVaga)
+{
+    // Validação para verificar se o objeto está vazio
+    if (!empty($dadosVaga)) {
+
+        // Validação para verificar se o objeto contém os dados obrigatórios
+        if (!empty($dadosVaga['numero']) && !empty($dadosVaga['idSetor']) && !empty($dadosVaga['idEstacionamento']) && !empty($dadosVaga['idEstacionamento']) && !empty($dadosVaga['idPlano'])) {
+
+            // Criação do array de dados que será encaminhado para a Model
+            $arrayDados = array(
+                'numero' => $dadosVaga['numero'],
+                'idSetor' => $dadosVaga['idSetor'],
+                'idEstacionamento' => $dadosVaga['idEstacionamento'],
+                'idPlano' => $dadosVaga['idPlano']
+            );               
+
+            // Import do arquivo de Model
+            require_once(SRC . 'vaga/model/bd/vaga.php');
+
+            // Chama e verifica sucesso da função da Model
+            if (insertVaga($arrayDados))
+                return true;
+            else
+                return array(
+                    'idErro'  => 1,
+                    'message' => 'Não foi possivel inserir os dados no Banco de Dados'
+                );
+        } else
+            return array(
+                'idErro'   => 2,
+                'message'  => 'Existem campos obrigatório que não foram preenchidos.'
+            );
+    }
+}
+
+// Função para receber dados da View e encaminhar para a model (função Update)
+function atualizarVaga($dadosVaga)
+{
+
+    // Recebe o ID enviado pelo array de parâmetro
+    $id = $dadosVaga['id'];
+
+    // Validação para verificar se o objeto está vazio
+    if (!empty($dadosVaga)) {
+
+        // Validação para verificar se o objeto contém os dados obrigatórios
+        if (!empty($dadosVaga['numero']) && !empty($dadosVaga['idSetor']) && !empty($dadosVaga['idEstacionamento']) && !empty($dadosVaga['idEstacionamento']) && !empty($dadosVaga['idPlano'])) {
+
+            //Validação para verificar se o ID é válido
+            if (!empty($id) && $id != 0 && is_numeric($id)) {
+
+                // Criação do array de dados que será encaminhado para a Model
+                $arrayDados = array(
+                    "id"        => $id,
+                    "numero"    => $dadosVaga[0]['numero'],
+                    "idSetor"   => $dadosVaga[0]['idSetor'],
+                    "idEstacionamento" => $dadosVaga[0]['idEstacionamento'],
+                    "idPlano"   => $dadosVaga[0]['idPlano']                    
+                );
+
+                // Import do arquivo de Model
+                require_once(SRC . 'vaga/model/bd/vaga.php');
+
+                // Chama e verifica sucesso da função da Model
+                if (updateVaga($arrayDados)) {
+                    return true;
+                } else
+                    return array(
+                        'idErro'  => 1,
+                        'message' => 'Não foi possivel atualizar os dados no Banco de Dados'
+                    );
+            } else
+                return array(
+                    'idErro'   => 4,
+                    'message'  => 'Não é possível editar um registro sem informar um id válido.'
+                );
+        } else
+            return array(
+                'idErro'   => 2,
+                'message'  => 'Existem campos obrigatório que não foram preenchidos.'
+            );
+    }
+}
+
+// Função para realizar a exclusão de um registro
+function excluirVaga($id)
+{
+
+    // Validação para verificar se o ID é válido
+    if ($id != 0 && !empty($id) && is_numeric($id)) {
+
+        // Import do arquivo de Model
+        require_once(SRC . 'vaga/model/bd/vaga.php');
+
+        // Import do arquivo de configurações do projeto
+        require_once(SRC . 'modulo/config.php');
+
+        // Chama e verifica sucesso da função da Model
+        if (deleteVaga($id)) {
+            return true;
+        } else
+            return array(
+                'idErro'   => 3,
+                'message'  => 'O banco de dados não pode excluir o registro.'
+            );
+    } else
+        return array(
+            'idErro'   => 4,
+            'message'  => 'Não é possível excluir um registro sem informar um id válido.'
+        );
+}
+
+// Função para solicitar os dados da Model e encaminhar a lista de registros para a View
+function listarVaga()
+{
+
+    // Import do arquivo de Model
+    require_once(SRC . 'vaga/model/bd/vaga.php');
+
+    // Solicita a função que vai buscar os dados no BD e armazena o retorno
+    $dados = listarAllVagas();
+
+    // Verifica se os dados tragos pela Model estão vazios para então retorná-los
+    if (!empty($dados))
+        return $dados;
+    else
+        return false;
+}
+
+// Função para buscar um registro através do seu ID
+function buscarVaga($id)
+{
+
+    // Validação para verificar se o ID é válido
+    if ($id != 0 && !empty($id) && is_numeric($id)) {
+
+        // Import do arquivo de Model
+        require_once(SRC . 'vaga/model/bd/vaga.php');
+
+        // Solicita a função que vai buscar os dados no BD e armazena o retorno
+        $dados = selectByIdVaga($id);
+
+        // Verifica se os dados tragos pela Model estão vazios para então retorná-los
+        if (!empty($dados))
+            return $dados;
+        else
+            return false;
+    } else
+        return array(
+            'idErro'   => 4,
+            'message'  => 'Não é possível buscar um registro sem informar um id válido.'
+        );
+}
---
+++ vaga/model/bd/vaga.php
@@ -0,0 +1,187 @@
+<?php
+
+/***********************************************************************
+ * Objetivo: Arquivo responsável por manipular os dados dentro do BD
+ *          (insert, update, select e delete).
+ * Autora: Vinicio
+ * Data: 01/06/2022
+ * Versão: 1.0
+ ***********************************************************************/
+
+// Import do arquivo para estabecer a conexão com o BD
+require_once('../modulo/conexaoMySql.php');
+
+// Função para realizar o insert de um registro no BD
+function insertVaga($dadosVaga)
+{
+    // Declaração de variavel para se utilizar no return
+    $statusResposta = (bool) false;
+
+    // Abre aconexão com o BD
+    $conexao = conexaoMysql();
+
+    // Script para adição de registro
+    $sql = "insert into tblVagas 
+                    (numero,
+                     idSetor,
+                     idEstacionamento, 
+                     idPlano                     
+                     )
+                values 
+                    ('" . $dadosVaga['numero'] . "',                     
+                    '" . $dadosVaga['idSetor'] . "',
+                    '" . $dadosVaga['idEstacionamento'] . "',
+                    '" . $dadosVaga['idPlano'] . "'
+                );";
+
+
+    //Validação para verificar se o script SQL está correto
+    if (mysqli_query($conexao, $sql)) {
+
+        // Validação para verificar se uma linha foi acrescentada no DB
+        if (mysqli_affected_rows($conexao))
+            $statusResposta = true;
+    }
+
+    // Fecha a conexão com o BD
+    fecharConexaoMysql($conexao);
+
+    return $statusResposta;
+}
+
+// Função para listar todos os registros no BD
+function listarAllVagas()
+{
+
+    // Abre aconexão com o BD
+    $conexao = conexaoMysql();
+
+    // Script para listar todos os dados do BD
+    $sql = "select * from tblVagas order by id desc;";
+
+    // Executa o script sql no BD e guarda o retorno dos dados
+    $result = mysqli_query($conexao, $sql);
+
+    // Valida se o BD retornou registros 
+    if ($result) {
+
+        $cont = 0;
+
+        // Converte os dados do BD em array
+        while ($rsDados = mysqli_fetch_assoc($result)) {
+            $arrayDados[$cont] = array(
+                "id"        =>  $rsDados['id'],
+                "numero"      =>  $rsDados['numero'],
+                "idSetor"     =>  $rsDados['idSetor'],
+                "idEstacionamento" =>  $rsDados['idEstacionamento'],
+                "idPlano"     =>  $rsDados['idPlano']                
+            );
+            $cont++;
+        }
+
+        // Fecha a conexão com o BD
+        fecharConexaoMysql($conexao);
+
+        // O script apenas foi um sucesso quando a variável arrayDados existir
+        if (isset($arrayDados))
+            return $arrayDados;
+        else
+            return false;
+    }
+}
+
+// Função para selecionar um registro no BD, pelo seu ID
+function selectByIdVaga($id)
+{
+
+    // Abre a conexao com o BD
+    $conexao = conexaoMysql();
+
+    // Script para listar todos os dados do BD, em ordem decrescente
+    $sql = "select * from tblVagas where id = " . $id;
+
+    // Executa o script sql no BD e guarda o retorno dos dados
+    $result = mysqli_query($conexao, $sql);
+
+    // Valida se o BD retornou registros 
+    if ($result) {
+
+        // Convertendo os dados do BD em array
+        if ($rsDados = mysqli_fetch_assoc($result)) {
+
+            $arrayDados = array(
+                "id"        =>  $rsDados['id'],
+                "numero"      =>  $rsDados['numero'],
+                "idSetor"     =>  $rsDados['idSetor'],
+                "idEstacionamento" =>  $rsDados['idEstacionamento'],
+                "idPlano"     =>  $rsDados['idPlano']
+            );
+
+            return $arrayDados;
+        } else {
+            return false;
+        }
+
+        // Fecha a conexão com o BD
+        fecharConexaoMysql($conexao);
+    }
+}
+
+// Função para atualizar registro no BD
+function updateVaga($dadosVaga)
+{
+
+    // Declaração de variavel para se utilizar no return
+    $status = (bool) false;
+
+    // Abre aconexão com o BD
+    $conexao = conexaoMysql();
+
+    // Script para atualizar um registro através de seu id
+    $sql = "update tblVagas set
+                        numero = " . $dadosVaga['numero'] . ", 
+                        idSetor = '" . $dadosVaga['idSetor'] . "',
+                        idEstacionamento = '" . $dadosVaga['idEstacionamento'] . "',
+                        idPlano = '" . $dadosVaga['idPlano'] . "'
+                        where id =" . $dadosVaga['id'];
+
+    // Validação para verificar se o script sql está correto
+    if (mysqli_query($conexao, $sql)) {
+
+        // Validacao para verificar se uma linha foi acrescentada no BD
+        if (mysqli_affected_rows($conexao))
+            $status = true;
+    }
+
+    // Fecha a conexão com o BD
+    fecharConexaoMysql($conexao);
+
+    return $status;
+}
+
+// Função para deletar registro no BD
+function deleteVaga($id)
+{
+
+    // Abre a conexao com o BD
+    $conexao = conexaoMySql();
+
+    // Declaração de variavel para se utilizar no return
+    $status = (bool) false;
+
+    // Script para deletar um registro através de seu ID
+    $sql = "delete from tblVagas where id =" . $id;
+
+    // Validação para verificar se o script sql está correto para executá-lo
+    if (mysqli_query($conexao, $sql)) {
+
+        // Validacao para verificar se uma linha foi acrescentada no BD
+        if (mysqli_affected_rows($conexao))
+            $status = true;
+    }
+
+    // Fecha a conexao com o BD
+    fecharConexaoMysql($conexao);
+
+    return $status;
+}
