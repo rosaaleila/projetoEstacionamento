fb50ccf002fa48b6de63ab21864c2677707d8261
VinicioJesus <viniciojesus298@gmail.com>
2022-06-10 17:00:04 -0300

    retornos do cliente APi ajustados

--- api/clienteAPI/index.php
+++ api/clienteAPI/index.php
@@ -10,22 +10,23 @@
  * Versão: 1.0
  ***********************************************************************/
 
-  //import do arquivo autoload, que fará as instancias do slim
+  //import do arquivo autoload, que fará as instâncias do slim
   require_once('vendor/autoload.php');  
 
   //Criando um objeto do slim chamado app, para coonfigurar os endpoints(rotas)
   $app = new \Slim\App();
 
+  // lista todos os
   $app->get('/clientes', function ($request, $response, $args) {
 
     // importa do arquivo de configuracao
     require_once('../modulo/config.php');
-    // import da controller de contatos, que fara a busca de dados
+    // import da controller de clientes, que fara a busca de dados
     require_once('../cliente/controller/controllerClientes.php');
 
-    // solicita os dados para a controller
+    // solicita os dados da controller
     if ($dados = listarCliente()) {
-      // realiza a conversao do array de dados em formato json
+      // realiza a conversão do array de dados em formato json
         if ($dadosJSON = createJSON($dados)) {
             // caso exista dados, retornamos o status code e enviamos os dados em json
             return $response
@@ -38,138 +39,130 @@
         return $response
             ->withStatus(404)
             ->withHeader('Content-Type', 'application/json')
-            ->write('{"idErro": "404", "message": "Não foi possivel encontrar registros."}');
+            ->write('{"idErro": "404", "message": "Não foi possivel encontrar clientes cadastrados."}');
     }
-});
+  });
 
-  //Endpoint Requisição para listar contatos pelo id
+  //Endpoint para listar um cliente pelo id
   $app->get('/clientes/{id}', function($request, $response, $args){
     
-    //Recebe o id do registro que devera ser retornado pela api
-    //Esse ID está chegando pela varável criada no endpoint
+    //Recebe o id do cliente que devera ser retornado pela api
     $id = $args['id'];
-     
-     //import da controller de contatos, que fará a busca de dados
+      
+    // importa do arquivo de configuracao
     require_once('../modulo/config.php');
+    //import da controller de contatos, que fará a busca de dados
     require_once('../cliente/controller/controllerClientes.php');
-    //solicita os dados para a controller
+    //solicita os dados da controller
     if($dados = buscarCliente($id))
     {
       if (!isset($dados['idErro'])) {
         
-        //realiza a conversão do array de dados para json
+        //realiza a conversão do array de dados para JSON
       if ($dadosJSON = createJSON($dados))
         {   
-            // Caso exista dados a serem retornados, informamos o statusCode 200 e
-            // enviamos um JSON com todos os dados encontrados
+          //caso exista dados, retornamos o status code e enviamos os dados em JSON
            return $response   ->withStatus(200)
                               ->withHeader('Content-Type', 'application/json')
                               ->write($dadosJSON);
         }
 
       }else {
-        $dadosJSON = createJSON($dados);
 
+        $dadosJSON = createJSON($dados);
+        //retorna um status code caso a solicitacao dê errado
         return $response  ->withStatus(404)
                           ->withHeader('Content-Type', 'application/json')
-                          ->write('{"message": "Dados inválidos",
+                          ->write('{"message": "id ou cliente inválido",
                           "Erro": '.$dadosJSON.' }');
                           
       }
     }else{
-
-      //retorna os statusCode que significa que a requisição foi aceita, porém 
-      //sem conteudo de retorno
+      //status code que o endpoint deu certo mas não encontrou o cliente      
       return $response   ->withStatus(204);
                   
     }
 
   });
 
-  //Endpoint Requisição para inserir um novo cliente
+  //Endpoint para inserir um novo cliente
   $app->post('/clientes', function($request, $response, $args){
     
-    //Recebe do header da requisição qual será o content type
+    //Recebe do header da requisição definindo qual será o content type
     $contentTypeHeader = $request->getHeaderLine('Content-Type');
-    //Cria um array ois dependendo do content-type temos mais informações separadas
+    //Cria um array  que dependendo do content-type teremos mais informações separadas
     $contentType = explode(';', $contentTypeHeader);
 
+    //Verifica se o content-type é json
     switch ($contentType[0]) {
       case 'application/json':
 
-        //Recebe os dados enviado pelo corpo da requisição
-        $dadosBody = $request->getParsedBody();   
+      //Recebe os dados enviado pelo corpo da requisição
+      $dadosBody = $request->getParsedBody();   
       
-      //import da controller de contatos, que fará a busca de dados
+      // importa do arquivo de configuracao
       require_once('../modulo/config.php');
+      // import da controller de clientes, que fara a busca de dados
       require_once('../cliente/controller/controllerClientes.php');
-      
+      //solicita os dados da controller
       $resposta = inserirCliente($dadosBody);
 
       if (is_int($resposta)) {
-
+        //caso a solicitacao dê certo, retornamos o status code e enviamos os dados em JSON
         return $response   ->withStatus(201)
                             ->withHeader('Content-Type', 'application/json')
                             ->write('{"message":"Cliente inserido com sucesso", "idCliente": '.$resposta.'}');
 
       } elseif (is_array($resposta) && $resposta['idErro'])        
       {
-        $dadosJSON = createJSON($resposta);
 
+        $dadosJSON = createJSON($resposta);
+        //caso a solicitacao dê errado, retornamos o status code e enviamos os dados em JSON
         return $response   ->withStatus(400)
                             ->withHeader('Content-Type', 'application/json')
-                            ->write('{"message":"Erro ao inserir contato."},
+                            ->write('{"message":"Erro ao inserir um cliente."},
                             "Erro": '.$dadosJSON.' }');
-      }     
-        
-        break;     
-      
+      }             
+      break;       
     }
 
   });
 
-  //Endpoint Requisição para atualizar um contato, simulando o PUT
+  //Endpoint para atualizar um cliente
   $app->put('/clientes/{id}', function($request, $response, $args){
       
-    //Recebe do header da requisição qual será o content type
+    //Recebe do header da requisição definindo qual será o content type
     $contentTypeHeader = $request->getHeaderLine('Content-Type');
-    //Cria um array ois dependendo do content-type temos mais informações separadas
+    //Cria um array que dependendo do content-type teremos mais informações separadas
     $contentType = explode(';', $contentTypeHeader);
 
     if(is_numeric($args['id']))
-    {
+    {     
+      //Recebe o id do cliente que devera ser atualizado pela api
+      $id = $args['id']; 
       
-      
-      //Recebe o id enviado no Endpoint atraves da vareavel ID
-      $id = $args['id'];  
-      
-      echo($id);
-      
-
       switch ($contentType[0]) {
-        case 'application/json':
-          //import da controller de contatos, que fará a busca de dados
+        
+          case 'application/json':
+          // importa do arquivo de configuracao
           require_once('../modulo/config.php');
+          // import da controller de clientes, que fara a busca de dados
           require_once('../cliente/controller/controllerClientes.php');
-
-          //chama a função para buscar a foto que ja está salva no banco de dados
-          
 
           $dadosBody = $request->getParsedBody();
             
-          //Cria um array com toods os dados couns e do do arquivo que será enviado para o servidor
+          //Cria um array com o id do registro que será atualizado
           $arrayDados = array( $dadosBody,                               
                               "id" => $id                              
           );
-        
-          
+                  
           $resposta = atualizarCliente($arrayDados);
 
           if (is_bool($resposta) && $resposta == true) {
 
             return $response   ->withStatus(200)
                                 ->withHeader('Content-Type', 'application/json')
-                                ->write('{"message":"Contato atualizado com sucesso"}');
+                                ->write('{"message":"Cliente atualizado com sucesso"}');
 
           } elseif (is_array($resposta) && $resposta['idErro'])        
           {
@@ -177,7 +170,7 @@
 
             return $response   ->withStatus(400)
                                 ->withHeader('Content-Type', 'application/json')
-                                ->write('{"message":"Erro ao atualizar contato."},
+                                ->write('{"message":"Erro ao atualizar o cliente."},
                                 "Erro": '.$dadosJSON.' }');
             break;      
           
@@ -193,7 +186,7 @@
     }
   });
 
-  //Endpoit: Requisição para deletar um contato
+  //Endpoit deletar um cliente
   $app->delete('/clientes/{id}', function($request, $response, $args){
 
 
@@ -252,7 +245,7 @@
 
   });
 
-  //Executa todos os Endpoint
+  
   $app->run();
   
 
